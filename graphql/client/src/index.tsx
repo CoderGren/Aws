a61b121d1c8695824d9e1eb23bcfbfb7fa9898bd
Glen <glen@unsuccessfultech.com>
2023-10-24 15:43:57 -0400

    Setup apollo client

--- graphql/client/src/index.tsx
+++ graphql/client/src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {ApolloProvider} from "@apollo/client";
+import {client} from "./apollo";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
